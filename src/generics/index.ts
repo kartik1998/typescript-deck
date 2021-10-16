31086f46a2f6b7c4c4d7c3abf841cb59ea68eaee
kartik1998 <kartikrawat61@gmail.com>
2021-10-16 18:23:17 +0530

    Add: setting constraints

--- src/generics/index.ts
+++ src/generics/index.ts
@@ -7,4 +7,13 @@ const mergedObj = merge({ name: 'Ken'}, {age: 30 });
 const sampleMerge = merge<string, number>('', 1);
 console.log(mergedObj);
 
+/* setting constraints */
+
+/**
+ * @T has to be an object
+ * @U has to be a number
+ */
+function mergeConstraint<T extends object, U extends number>(a: T, b: U) {
+    return {a, b};
+}
 
